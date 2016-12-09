58c763e52dfeb28fa092eccade65cd03c5b0c65b
Hugh Liu <hughliu@Hughs-MacBook-Pro.local>
2016-12-09 23:39:47 +1100

    added blog model

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,11 @@
 from __future__ import unicode_literals
 
-from django.db import models
-
 # Create your models here.
+from google.appengine.ext import ndb
+
+class Blog(ndb.Model):
+    title = ndb.StringProperty(required=True)
+    slug = ndb.StringProperty(required=True)
+    content = ndb.TextProperty(required=True)
+    posted = ndb.DateTimeProperty(auto_now_add=True)
+
