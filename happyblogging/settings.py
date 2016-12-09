de77af36417d49337dc8875f47cf6da76fefc3d0
Hugh Liu <hughliu@Hughs-MacBook-Pro.local>
2016-12-09 23:00:15 +1100

    fixed init commit

--- happyblogging/settings.py
+++ happyblogging/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'happyblogging.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(os.path.dirname(__file__), 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
